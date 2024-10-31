83018c7befbe7b63d86b50cdb48583645f31c894
Mohamed EL GAZI <126195340+elgazimohamed@users.noreply.github.com>
2024-10-31 11:11:55 +0100

    WeatherNow Web App

--- obj/Debug/net8.0/WeatherNow.AssemblyInfo.cs
+++ obj/Debug/net8.0/WeatherNow.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherNow")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5841a3ebd1985f0abda203653ff3b000f5f04b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherNow")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherNow")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
